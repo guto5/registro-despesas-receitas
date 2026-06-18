7cab994e3920122682130dc5e04f450a252c9250
guto5 <guto.cezar2005@gmail.com>
2026-06-18 20:13:38 -0300

    teste

--- backend/prisma/migrations/20260331120001_seed_categoria/migration.sql
+++ backend/prisma/migrations/20260331120001_seed_categoria/migration.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "categoria" (
+CREATE TABLE "tipo" (
                         "id" UUID NOT NULL,
                         "nome" TEXT NOT NULL,
                         CONSTRAINT "tipo_pkey" PRIMARY KEY ("id")
