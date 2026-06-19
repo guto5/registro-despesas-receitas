d3c4682ea83dfdd5619300da1e5c5a4a0966c635
guto5 <guto.cezar2005@gmail.com>
2026-06-18 21:36:09 -0300

    testt

---
+++ backend/prisma/migrations/20260331120004_seed_tipo/migration.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "tipos" (
+                        "id" UUID NOT NULL,
+                        "nome" TEXT NOT NULL,
+                        CONSTRAINT "tipos_pkey" PRIMARY KEY ("id")
+);
